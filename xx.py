eb56f69a7557904ef26a4efb8e160f8f88544fff
xtwxfxk <xtwxfxk@163.com>
2018-03-22 08:45:25 +0800

    fvck

---
+++ xx.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+__author__ = 'xtwxfxk'
+
+import tornado.ioloop
+import tornado.web
+
+from flask import Flask
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        self.write("Hello, world")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(8888)
+    tornado.ioloop.IOLoop.current().start()
