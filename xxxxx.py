9928c7f323765d6f00209f906adb03d768220508
xtwxfxk <xtwxfxk@163.com>
2018-06-27 14:51:06 +0800

    fvck

--- xxxxx.py
+++ xxxxx.py
@@ -7,3 +7,10 @@ import torchvision
 torchvision.utils.save_image()
 
 torchvision.datasets
+
+from tensorboardX import SummaryWriter
+
+writer = SummaryWriter()
+writer.add_scalar()
+
+import torch.nn.functional
