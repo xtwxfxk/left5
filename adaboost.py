ee324e98c53ce7252d87d4cdb45d3e096b2b7c76
zhuaqiang <zhuaqiang@linewell.com>
2020-06-01 10:18:56 +0800

    0601

---
+++ adaboost.py
@@ -0,0 +1,171 @@
+
+
+'''https://xavierbourretsicotte.github.io/AdaBoost.html'''
+
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+import numpy as np
+import seaborn as sns
+# sns.set_style('white')
+
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import AdaBoostClassifier
+
+import ipdb
+
+
+#Toy Dataset
+x1 = np.array([.1,.2,.4,.8, .8, .05,.08,.12,.33,.55,.66,.77,.88,.2,.3,.4,.5,.6,.25,.3,.5,.7,.6])
+x2 = np.array([.2,.65,.7,.6, .3,.1,.4,.66,.77,.65,.68,.55,.44,.1,.3,.4,.3,.15,.15,.5,.55,.2,.4])
+y = np.array([1,1,1,1,1,1,1,1,1,1,1,1,1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1])
+X = np.vstack((x1,x2)).T
+
+def plot_decision_boundary(classifier, X, y, N = 10, scatter_weights = np.ones(len(y)) , ax = None ):
+    '''Utility function to plot decision boundary and scatter plot of data'''
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid( np.linspace(x_min, x_max, N), np.linspace(y_min, y_max, N))
+
+    
+    #Check what methods are available
+    if hasattr(classifier, "decision_function"):
+        zz = np.array( [classifier.decision_function(np.array([xi,yi]).reshape(1,-1)) for  xi, yi in zip(np.ravel(xx), np.ravel(yy)) ] )
+    elif hasattr(classifier, "predict_proba"):
+        zz = np.array( [classifier.predict_proba(np.array([xi,yi]).reshape(1,-1))[:,1] for  xi, yi in zip(np.ravel(xx), np.ravel(yy)) ] )
+    else :
+        zz = np.array( [classifier(np.array([xi,yi]).reshape(1,-1)) for  xi, yi in zip(np.ravel(xx), np.ravel(yy)) ] )
+            
+    # reshape result and plot
+    Z = zz.reshape(xx.shape)
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    
+    #Get current axis and plot
+    if ax is None:
+        ax = plt.gca()
+    ax.contourf(xx, yy, Z, 2, cmap='RdBu', alpha=.5)
+    ax.contour(xx, yy, Z,  2, cmap='RdBu')
+    ax.scatter(X[:,0],X[:,1], c = y, cmap = cm_bright, s = scatter_weights * 40)
+    ax.set_xlabel('$X_1$')
+    ax.set_ylabel('$X_2$')
+
+# boost = AdaBoostClassifier( base_estimator = DecisionTreeClassifier(max_depth = 1, max_leaf_nodes=2), 
+#                             algorithm = 'SAMME',n_estimators=10, learning_rate=1.0)
+# boost.fit(X,y)
+# plot_decision_boundary(boost, X,y, N = 50)#, weights)
+# plt.show()
+
+# boost.score(X,y)
+
+def AdaBoost_scratch(X,y, M=10, learning_rate = 1):
+    #Initialization of utility variables
+    N = len(y)
+    estimator_list, y_predict_list, estimator_error_list, estimator_weight_list, sample_weight_list = [], [],[],[],[]
+
+    #Initialize the sample weights
+    sample_weight = np.ones(N) / N
+    sample_weight_list.append(sample_weight.copy())
+
+    #For m = 1 to M
+    for m in range(M):   
+        ipdb.set_trace()
+        #Fit a classifier
+        estimator = DecisionTreeClassifier(max_depth = 1, max_leaf_nodes=2)
+        estimator.fit(X, y, sample_weight=sample_weight)
+        y_predict = estimator.predict(X)
+
+        #Misclassifications
+        incorrect = (y_predict != y)
+
+        #Estimator error
+        estimator_error = np.mean( np.average(incorrect, weights=sample_weight, axis=0))
+        
+        #Boost estimator weights
+        estimator_weight =  learning_rate * np.log((1. - estimator_error) / estimator_error)
+
+        #Boost sample weights
+        sample_weight *= np.exp(estimator_weight * incorrect * ((sample_weight > 0) | (estimator_weight < 0)))
+
+        #Save iteration values
+        estimator_list.append(estimator)
+        y_predict_list.append(y_predict.copy())
+        estimator_error_list.append(estimator_error.copy())
+        estimator_weight_list.append(estimator_weight.copy())
+        sample_weight_list.append(sample_weight.copy())
+        
+
+
+    #Convert to np array for convenience   
+    estimator_list = np.asarray(estimator_list)
+    y_predict_list = np.asarray(y_predict_list)
+    estimator_error_list = np.asarray(estimator_error_list)
+    estimator_weight_list = np.asarray(estimator_weight_list)
+    sample_weight_list = np.asarray(sample_weight_list)
+
+    #Predictions
+    preds = (np.array([np.sign((y_predict_list[:,point] * estimator_weight_list).sum()) for point in range(N)]))
+    print('Accuracy = ', (preds == y).sum() / N) 
+    
+    return estimator_list, estimator_weight_list, sample_weight_list
+
+
+estimator_list, estimator_weight_list, sample_weight_list  = AdaBoost_scratch(X,y, M=10, learning_rate = 1)
+
+
+def plot_AdaBoost_scratch_boundary(estimators,estimator_weights, X, y, N = 10,ax = None ):
+    
+    def AdaBoost_scratch_classify(x_temp, est,est_weights ):
+        '''Return classification prediction for a given point X and a previously fitted AdaBoost'''
+        temp_pred = np.asarray( [ (e.predict(x_temp)).T* w for e, w in zip(est,est_weights )]  ) / est_weights.sum()
+        return np.sign(temp_pred.sum(axis = 0))
+    
+    
+    '''Utility function to plot decision boundary and scatter plot of data'''
+    x_min, x_max = X[:, 0].min() - .1, X[:, 0].max() + .1
+    y_min, y_max = X[:, 1].min() - .1, X[:, 1].max() + .1
+    xx, yy = np.meshgrid( np.linspace(x_min, x_max, N), np.linspace(y_min, y_max, N))
+
+
+    zz = np.array( [AdaBoost_scratch_classify(np.array([xi,yi]).reshape(1,-1), estimators,estimator_weights ) for  xi, yi in zip(np.ravel(xx), np.ravel(yy)) ] )
+            
+    # reshape result and plot
+    Z = zz.reshape(xx.shape)
+    cm_bright = ListedColormap(['#FF0000', '#0000FF'])
+    
+    if ax is None:
+        ax = plt.gca()
+    ax.contourf(xx, yy, Z, 2, cmap='RdBu', alpha=.5)
+    ax.contour(xx, yy, Z,  2, cmap='RdBu')
+    ax.scatter(X[:,0],X[:,1], c = y, cmap = cm_bright)
+    ax.set_xlabel('$X_1$')
+    ax.set_ylabel('$X_2$')
+
+
+plot_AdaBoost_scratch_boundary(estimator_list, estimator_weight_list, X, y, N = 50 )
+
+
+estimator_list, estimator_weight_list, sample_weight_list  = AdaBoost_scratch(X,y, M=10, learning_rate = 1)
+
+fig = plt.figure(figsize = (14,14))
+for m in range(0,9):
+    fig.add_subplot(3,3,m+1)
+    s_weights = (sample_weight_list[m,:] / sample_weight_list[m,:].sum() ) * 40
+    plot_decision_boundary(estimator_list[m], X,y,N = 50, scatter_weights =s_weights )
+    plt.title('Estimator decision boundary, m = {}'.format(m))
+
+
+fig = plt.figure(figsize = (15,10))
+for k,l in enumerate([0.1,0.5,1]):
+    fig.add_subplot(2,3,k+1)
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    estimator_list, estimator_weight_list, sample_weight_list = AdaBoost_scratch(X,y, M=10, learning_rate = l)
+    plot_AdaBoost_scratch_boundary(estimator_list,estimator_weight_list, X, y, N = 50,ax = None )
+    plt.title('Adaboost boundary: M = 10,  L = {}'.format(l))
+    print(estimator_weight_list)
+
+for k,m in enumerate([1,3,10]):
+    fig.add_subplot(2,3,k+4)
+    fig.subplots_adjust(hspace=0.4, wspace=0.4)
+    estimator_list, estimator_weight_list, sample_weight_list = AdaBoost_scratch(X,y, M=m, learning_rate = 1)
+    plot_AdaBoost_scratch_boundary(estimator_list,estimator_weight_list, X, y, N = 50,ax = None )
+    plt.title('Adaboost boundary: M = {},  L = 1'.format(m))
+    print(estimator_weight_list)
