b9c0e8cfa3ddcea9506ab369ce20555a35441f72
xtwxfxk <xtwxfxk@163.com>
2020-08-09 13:54:06 +0800

    0809

--- fb/fb_group_pic.py
+++ fb/fb_group_pic.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 __author__ = 'xtwxfxk'
 
-import time, csv, yaml
+import os, time, csv, yaml
 from io import BytesIO
-from PIL import Image
+from PIL import Image, ImageFilter
 from docx import Document
 from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
@@ -15,12 +15,10 @@ config = yaml.safe_load(open('config.yml').read())
 def read_keywords():
     keywords = []
     for row in csv.reader(open('keyworkds.csv'), delimiter=','):
-        if len(row) == 3 and row[1].isdigit() and row[2].isdigit():
-            keywords.append([row[0], int(row[1]), float(row[1])])
-        elif len(row) == 2 and row[1].isdigit():
-            keywords.append([row[0], int(row[1]), config['pic_alpha']])
+        if len(row) == 2 and row[1].isdigit():
+            keywords.append([row[0], int(row[1])])
         else:
-            keywords.append([row[0], int(config['pic_count']), float(config['pic_alpha'])])
+            keywords.append([row[0], int(config['pic_count'])])
 
     return keywords
 
@@ -28,7 +26,7 @@ def fetch_image(keywords):
     opts = Options()
     opts.add_argument("user-data-dir=%s" % config['profile_dir']) # D:/profiles/xxxxxx
     # opts.add_argument("profile-directory=%s" % config['profile_dir'])  # D:/profiles/xxxxxx
-    if config.get('proxy', None) is not None and config.get('config'):
+    if config.get('proxy', None) is not None and config.get('proxy'):
         opts.add_argument('proxy-server=%s' % config['proxy']) # socks5://127.0.0.1:1080
     browser = webdriver.Chrome(options=opts)
 
@@ -36,7 +34,10 @@ def fetch_image(keywords):
 
     browser.get('http://www.facebook.com')
 
-    for keyword, pic_count, pic_alpha in keywords:
+    if not os.path.exists('docx'):
+        os.mkdir('docx')
+
+    for keyword, pic_count in keywords:
         browser.get('http://www.facebook.com')
         doc = Document()
 
@@ -48,8 +49,8 @@ def fetch_image(keywords):
         search.send_keys(keyword)
         search.send_keys(Keys.ENTER)
 
-        wait.until(lambda driver: driver.find_element_by_xpath('//span[text()="你的小组"]'))
-        group = browser.find_element_by_xpath('//span[text()="你的小组"]')
+        wait.until(lambda driver: driver.find_element_by_xpath('//span[text()="你的小组" or text()="Your Groups"]'))
+        group = browser.find_element_by_xpath('//span[text()="你的小组" or text()="Your Groups"]')
         group.click()
 
         body = browser.find_element_by_xpath('//body')
@@ -71,32 +72,41 @@ def fetch_image(keywords):
         body.send_keys(Keys.HOME)
         time.sleep(2)
         for i, ele in enumerate(eles[:pic_count]):
+            img = Image.open(BytesIO(ele.screenshot_as_png))
 
-            blacks = browser.find_elements_by_xpath('//div[@class="_4rmu"]/div/div[1]')
-            for b in blacks:
-                browser.execute_script('arguments[0].setAttribute("style", "background-color:#DCDCDC")', b)
-            trs = ele.find_elements_by_xpath('//div[@class="_4rmu"]/div/div[1]//*')
-            # browser.execute_script('arguments[0].setAttribute("style", "color: rgba(75, 79, 86, 1)")', t)
+            blacks = ele.find_elements_by_xpath('./div/div[1]')
+
+            url_localtion = blacks[0].location
+            url_size = blacks[0].size
+            url_box = [0, 0, url_size['width'], url_size['height']]
+
+            hightNodes = []
+            trs = ele.find_elements_by_xpath('.//span[@class="highlightNode"]')
             for t in trs:
-                if 'highlightNode' not in t.get_attribute("class"):
-                    # browser.execute_script('arguments[0].setAttribute("style", "color: transparent;text-shadow: #111 0 0 4px;")', t)
-                    browser.execute_script('arguments[0].setAttribute("style", "color: rgba(75, 79, 86, %s)")' % pic_alpha, t)
-                    # browser.execute_script('arguments[0].setAttribute("style", "visibility: hidden;")', t)
-                else:
-                    # browser.execute_script('arguments[0].setAttribute("style", "font-weight:bold;color: black;text-shadow: #000 0 0 0px;")', t)
-                    browser.execute_script('arguments[0].setAttribute("style", "color: rgba(75, 79, 86, 1)")', t)
+                left = t.location['x'] - url_localtion['x']
+                upper = t.location['y'] - url_localtion['y']
+                right = left + t.size['width']
+                lower = upper + t.size['height']
+                box = [left, upper, right, lower]
+                hightNodes.append([box, img.crop(box)])
 
-            # hls = ele.find_elements_by_xpath('//div[@class="_4rmu"]//span[@class="highlightNode"]')
-            # for h in hls:
-            #     browser.execute_script('arguments[0].setAttribute("style", "color: transparent;text-shadow: none")', h)
+            url_img = img.crop(url_box)
+            if config.get('blur', None) and str(config.get('blur')).isdigit():
+                url_img = url_img.filter(ImageFilter.BoxBlur(config.get('blur')))
+            if config.get('watermark', None):
+                watermark = Image.open(config.get('watermark'))
+                url_img.paste(watermark, mask=watermark)
 
-            # img = Image.open(BytesIO(ele.screenshot_as_png))
+            img.paste(url_img, url_box)
+            for box, im in hightNodes:
+                img.paste(im, box)
 
-            # img.save("D:/code/python/fb/%s.png" % i, "PNG")
+            buf = BytesIO()
+            img.save(buf, "PNG")
 
-            doc.add_picture(BytesIO(ele.screenshot_as_png))
+            doc.add_picture(buf)
 
-        doc.save('%s.docx' % keyword)
+        doc.save('docx/%s.docx' % keyword)
 
 
 if __name__ == '__main__':
