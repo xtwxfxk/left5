2bf3d093055862cbcfdb1611817174b6cfec557a
left5 <zhanghuaqiang@dcorepay.com>
2020-12-29 11:20:08 +0800

    '1229'

--- amazon/amazon_product/xxx.py
+++ amazon/amazon_product/xxx.py
@@ -1,14 +1,21 @@
 # -*- coding: utf-8 -*-
 __author__ = 'xtwxfxk'
 
+import urllib.parse
 from lutils.lrequest import LRequest
 
-url = 'https://www.amazon.com/Best-Sellers-Home-Kitchen/zgbs/home-garden/ref=zg_bs_nav_0'
-
+# url = 'https://www.amazon.com/Best-Sellers-Home-Kitchen/zgbs/home-garden/ref=zg_bs_nav_0'
+url = 'https://www.amazon.com/Best-Sellers-Home-Kitchen-Décor-Products/zgbs/home-garden/1063278'
+# url = urllib.parse.quote('https://www.amazon.com/Best-Sellers-Home-Kitchen-Décor-Products/zgbs/home-garden/1063278')
+# url = urllib.parse.urlencode('https://www.amazon.com/Best-Sellers-Home-Kitchen-Décor-Products/zgbs/home-garden/1063278')
+url = urllib.parse.quote(url, safe='https:/')
+print(url)
 lr = LRequest()
 
-lr.load(url, isdecode=True)
-eles = lr.xpaths('//ul[@id="zg_browseRoot"]/ul/ul/li/a')
+lr.load(url, is_decode=True)
+eles = lr.xpaths('//ul[@id="zg_browseRoot"]/ul/ul/ul/li/a')
 
 for ele in eles:
     print(ele.text.strip(), ele.attrib['href'])
+
+# https://www.amazon.com/Best-Sellers-Home-Kitchen-D%C3%A9cor-Products/zgbs/home-garden/1063278/
