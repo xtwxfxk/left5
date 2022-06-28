a85af5b45f6fa4b25d2620409d4c94543a32d417
left5 <xtwxfxk@163.com>
2022-06-28 14:09:27 +0800

    0628

--- amazon/xxx/am.py
+++ amazon/xxx/am.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 __author__ = 'xtwxfxk'
 
-import os, time, csv
+import os, time, csv, traceback
 from urllib.parse import urljoin, quote
 import logging
 import logging.config
 from lutils.lrequests import LRequests
-
+from concurrent.futures import ThreadPoolExecutor
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger('verbose')
@@ -17,39 +17,52 @@ KEYWORD_PATH = os.path.join(ROOT, 'keywords')
 LOG_DIR = os.path.join(ROOT, 'logs')
 if not os.path.exists(LOG_DIR): os.makedirs(LOG_DIR)
 
-ASIN_DIR = os.path.join(ROOT, 'asin')
+ASIN_DIR = os.path.join(ROOT, 'tmp', 'asin')
 if not os.path.exists(ASIN_DIR): os.makedirs(ASIN_DIR)
 
-PRODUCTS_DIR = os.path.join(ROOT, 'products')
+PRODUCTS_DIR = os.path.join(ROOT, 'tmp', 'products')
 if not os.path.exists(PRODUCTS_DIR): os.makedirs(PRODUCTS_DIR)
 
-TRADEMARKIA_DIR = os.path.join(ROOT, 'trademarkia')
+TRADEMARKIA_DIR = os.path.join(ROOT, 'tmp', 'trademarkia')
 if not os.path.exists(TRADEMARKIA_DIR): os.makedirs(TRADEMARKIA_DIR)
 
-USPTO_DIR = os.path.join(ROOT, 'uspto')
+USPTO_DIR = os.path.join(ROOT, 'tmp', 'uspto')
 if not os.path.exists(USPTO_DIR): os.makedirs(USPTO_DIR)
 
+CSV_DIR = os.path.join(ROOT, 'csv')
+if not os.path.exists(CSV_DIR): os.makedirs(CSV_DIR)
 
 class SpiderAmazon():
 
-    def __init__(self, keywords=[]):
-        self.keywords = keywords
+    def __init__(self):
+
         self.lr = LRequests()
         self.key_asins = {}
 
+    def load_amazon(self, url):
+        self.lr.load(url)
+        if self.lr.body.find('Enter the characters you see below') > 0:
+            logger.error("Captcha!!!")
+            time.sleep(10)
+            self.lr = LRequests()
+            self.lr.load(url)
 
-    def fetch_list(self):
-        for keyword in self.keywords:
+    def fetch_list(self, keyword):
+
+        file = os.path.join(ASIN_DIR, '%s.txt' % keyword)
+        if not os.path.exists(file):
             self.key_asins[keyword] = []
 
-            self.lr.load('https://www.amazon.com/s?k=%s' % keyword)
+            self.lr.load('https://www.amazon.com/s?k=%s' % quote(keyword))
 
             while 1:
                 self.fetch_asin(keyword)
                 if not self.next_page():
                     break
 
-            open(os.path.join(ASIN_DIR, '%s.txt' % keyword), 'w').write('\n'.join(self.key_asins[keyword]))
+            open(file, 'w', encoding='utf-8').write('\n'.join(self.key_asins[keyword]))
+        else:
+            logger.info('pass keyword: %s' % keyword)
 
     def fetch_asin(self, keyword):
         product_eles = self.lr.xpaths('//div[contains(@class, "s-result-list")]/div[contains(@data-component-type, "s-search-result")]')
@@ -67,41 +80,57 @@ class SpiderAmazon():
             self.lr.load(next_url)
             return True
 
-    def fetch_asins(self):
-        for file in os.listdir(ASIN_DIR):
-            keyword = file[:-4]
-            if not os.path.exists(os.path.join(PRODUCTS_DIR, keyword)): os.makedirs(os.path.join(PRODUCTS_DIR, keyword))
-            for asin in open(os.path.join(ASIN_DIR, file)).readlines():
+    def fetch_asins(self, keyword):
+        if not os.path.exists(os.path.join(PRODUCTS_DIR, keyword)): os.makedirs(os.path.join(PRODUCTS_DIR, keyword))
+        for asin in open(os.path.join(ASIN_DIR, '%s.txt' % keyword), 'r', encoding='utf-8').readlines():
+            try:
                 asin = asin.strip()
                 if not os.path.exists(os.path.join(PRODUCTS_DIR, keyword, '%s.txt' % asin)):
                     self.fetch_product(keyword, asin)
                 else:
                     logger.info('pass %s' % asin)
+            except Exception as ex:
+                logger.error(ex, exc_info=True)
 
     def fetch_product(self, keyword, asin):
-        self.lr.load('https://www.amazon.com/dp/%s' % asin)
+        try:
+            # self.lr.load('https://www.amazon.com/dp/%s' % asin)
+            self.load_amazon('https://www.amazon.com/dp/%s' % asin)
 
-        title_ele = self.lr.xpath('//span[@id="productTitle"]')
-        title = ''.join(title_ele.itertext()).strip()
+            title_ele = self.lr.xpath('//span[@id="productTitle"]')
+            title = ''.join(title_ele.itertext()).strip()
 
-        detail_name_eles = self.lr.xpaths('//table[contains(@id, "productDetails")]//th')
-        detail_value_eles = self.lr.xpaths('//table[contains(@id, "productDetails")]//td')
+            manufacturer = ''
+            detail_name_eles = self.lr.xpaths('//table[contains(@id, "productDetails")]//th')
+            detail_value_eles = self.lr.xpaths('//table[contains(@id, "productDetails")]//td')
 
-        manufacturer_index = -1
-        for i, detail_name_ele in enumerate(detail_name_eles):
-            detail_text = ''.join(detail_name_ele.itertext()).strip().lower()
+            if detail_name_eles is None or len(detail_name_eles) < 1:
+                detail_name_eles = self.lr.xpaths('//div[contains(@id, "detailBullets_feature_div")]/ul[contains(@class, "detail-bullet-list")]//li/span/span[1]')
+                detail_value_eles = self.lr.xpaths('//div[contains(@id, "detailBullets_feature_div")]/ul[contains(@class, "detail-bullet-list")]//li/span/span[2]')
 
-            if detail_text == 'manufacturer':
-                manufacturer_index = i
-                break
 
-        manufacturer = ''
-        if manufacturer_index > -1:
-            manufacturer = ''.join(detail_value_eles[manufacturer_index].itertext()).strip().strip('‎')
+            manufacturer_index = -1
+            for i, detail_name_ele in enumerate(detail_name_eles):
+                detail_text = ''.join(detail_name_ele.itertext()).strip().lower()
 
-        open(os.path.join(PRODUCTS_DIR, keyword, '%s.txt' % asin), 'w').write('|||'.join([asin, manufacturer, title]))
+                if detail_text.find('manufacturer') > -1 and detail_text.find('by') < 1:
+                    manufacturer_index = i
+                    break
 
-    def fetch_uspto(self):
+            if manufacturer_index > -1:
+                manufacturer = ''.join(detail_value_eles[manufacturer_index].itertext()).strip().strip('\\n').strip('‎').strip()
+
+
+
+            open(os.path.join(PRODUCTS_DIR, keyword, '%s.txt' % asin), 'w', encoding='utf-8').write('|||'.join([asin, manufacturer, title]))
+        except Exception as ex:
+            open('xx\\%s.html' % time.time(), 'w', encoding='utf-8').write(str(self.lr.body))
+            logger.error(ex, exc_info=True)
+
+    def fetch_uspto(self, keyword):
+        uspth_path = os.path.join(USPTO_DIR, keyword)
+        if not os.path.exists(uspth_path):
+            os.makedirs(uspth_path)
         self.lr.load('https://tmsearch.uspto.gov/')
         search_ele = self.lr.xpath('//a[contains(@href, "searchss")]')
         if search_ele is not None:
@@ -109,86 +138,128 @@ class SpiderAmazon():
             # form = self.lr.get_forms()[0]
             # form = self.lr.getForms(urljoin(self.lr.current_url, search_ele.get('href')))[0]
 
-            for keyword in os.listdir(PRODUCTS_DIR):
-                key_path = os.path.join(PRODUCTS_DIR, keyword)
-                if os.path.isdir(key_path):
-                    for file in os.listdir(key_path):
-                        asin, manufacturer, title = open(os.path.join(key_path, file)).read().strip().split('|||')
-                        if manufacturer:
-                            state_ele = self.lr.xpath('//input[@name="state"]')
-                            state = state_ele.get('value')
-
-                            payload = {'f': 'toc', 
-                            'state': state,
-                            'p_search': 'search',
-                            'p_s_All': '',
-                            'p_s_ALL': manufacturer,
-                            'a_default': 'search',
-                            'a_search': 'Submit',}
-                            # self.lr.post('https://tmsearch.uspto.gov/bin/showfield', data=payload)
-
-                            self.lr.load('https://tmsearch.uspto.gov/bin/showfield', method='POST', data=payload)
-
-                            # self.lr.load('https://tmsearch.uspto.gov/bin/showfield?f=toc&state=%s&p_search=searchss&p_L=50&BackReference=&p_plural=yes&p_s_PARA1=&p_tagrepl%%7E%%3A=PARA1%%24LD&expr=PARA1+AND+PARA2&p_s_PARA2=%s&p_tagrepl%%7E%%3A=PARA2%%24COMB&p_op_ALL=AND&a_default=search&a_search=Submit+Query&a_search=Submit+Query' % (quote(state), quote(manufacturer)))
-                            # form['p_s_PARA2'] = manufacturer
-                            # self.lr.load(form.click())
-                            eles = self.lr.xpaths('//table[@id="searchResultTable"]//tr')
-                            print(len(eles))
-                            open('xx.html', 'w').write(str(self.lr.body))
-                            if eles is not None and len(eles) > 1:
-                                logger.info('Manufacturer %s: %s' % (manufacturer, len(eles)))
-                                open(os.path.join(USPTO_DIR, '%s.txt' % asin), 'w').write(str(len(eles)))
-                            else:
-                                logger.info('Manufacturer %s: None' % manufacturer)
-                                open(os.path.join(USPTO_DIR, '%s.txt' % asin), 'w').write("0")
-                        else:
-                            logger.info('Pass Empty Manufacturer %s' % asin)
-
-
-    def fetch_trademarkia(self):
-        for keyword in os.listdir(PRODUCTS_DIR):
             key_path = os.path.join(PRODUCTS_DIR, keyword)
             if os.path.isdir(key_path):
                 for file in os.listdir(key_path):
-                    asin, manufacturer, title = open(os.path.join(key_path, file)).read().strip().split('|||')
+                    try:
+                        asin, manufacturer, title = open(os.path.join(key_path, file), 'r', encoding='utf-8').read().strip().split('|||')
+                        uspto_file = os.path.join(uspth_path, '%s.txt' % asin)
+                        if not os.path.exists(uspto_file):
+                            if manufacturer:
+                                state_ele = self.lr.xpath('//input[@name="state"]')
+                                state = state_ele.get('value')
 
-                    if manufacturer:
-                        self.lr.load('https://www.trademarkia.com/trademarks-search.aspx?tn=%s' % quote(manufacturer))
-                        eles = self.lr.xpaths('//table[contains(@class, "tablesaw")]//tr')
-                        if eles is not None and len(eles) > 1:
-                            logger.info('Manufacturer %s: %s' % (manufacturer, len(eles)))
-                            open(os.path.join(TRADEMARKIA_DIR, '%s.txt' % asin), 'w').write(str(len(eles)))
+                                payload = {'f': 'toc', 
+                                'state': state,
+                                'p_search': 'search',
+                                'p_s_All': '',
+                                'p_s_ALL': manufacturer,
+                                'a_default': 'search',
+                                'a_search': 'Submit',}
+
+                                self.lr.load('https://tmsearch.uspto.gov/bin/showfield', method='POST', data=payload)
+
+                                eles = self.lr.xpaths('//table[@id="searchResultTable"]//tr')
+                                if eles is not None and len(eles) > 1:
+                                    logger.info('Manufacturer %s: %s' % (manufacturer, len(eles)))
+                                    open(uspto_file, 'w', encoding='utf-8').write(str(len(eles)))
+                                else:
+                                    logger.info('Manufacturer %s: None' % manufacturer)
+                                    open(uspto_file, 'w', encoding='utf-8').write("0")
+                            else:
+                                logger.info('Pass Empty Manufacturer %s' % asin)
                         else:
-                            logger.info('Manufacturer %s: None' % manufacturer)
-                            open(os.path.join(TRADEMARKIA_DIR, '%s.txt' % asin), 'w').write("0")
+                            logger.info('Pass Empty Uspto %s' % asin)
+                    except Exception as ex:
+                        logger.error(ex, exc_info=True)
+
+
+    def fetch_trademarkia(self, keyword):
+        trademarkia_path = os.path.join(TRADEMARKIA_DIR, keyword)
+        if not os.path.exists(trademarkia_path):
+            os.makedirs(trademarkia_path)
+        key_path = os.path.join(PRODUCTS_DIR, keyword)
+        if os.path.isdir(key_path):
+            for file in os.listdir(key_path):
+                try:
+                    asin, manufacturer, title = open(os.path.join(key_path, file), 'r', encoding='utf-8').read().strip().split('|||')
+                    trademarkia_file = os.path.join(trademarkia_path, '%s.txt' % asin)
+                    if not os.path.exists(trademarkia_file):
+                        if manufacturer:
+                            self.lr.load('https://www.trademarkia.com/trademarks-search.aspx?tn=%s' % quote(manufacturer))
+                            eles = self.lr.xpaths('//table[contains(@class, "tablesaw")]//tr')
+                            if eles is not None and len(eles) > 1:
+                                logger.info('Manufacturer %s: %s' % (manufacturer, len(eles)))
+                                open(trademarkia_file, 'w', encoding='utf-8').write(str(len(eles)))
+                            else:
+                                logger.info('Manufacturer %s: None' % manufacturer)
+                                open(trademarkia_file, 'w', encoding='utf-8').write("0")
+                        else:
+                            logger.info('Pass Empty Manufacturer %s' % asin)
                     else:
-                        logger.info('Pass Empty Manufacturer %s' % asin)
+                        logger.info('Pass Trademarkia %s' % asin)
+                except Exception as ex:
+                    logger.error(ex, exc_info=True)
+
+    def output_csv(self, keyword):
+        product_dir = os.path.join(PRODUCTS_DIR, keyword)
+
+        products = []
+        for file in os.listdir(product_dir):
+            asin, manufacturer, title = open(os.path.join(product_dir, file), 'r', encoding='utf-8').read().strip().split('|||')
+
+            t_m = '否'
+            t_dir = os.path.join(TRADEMARKIA_DIR, keyword, '%s.txt' % asin)
+            if os.path.exists(t_dir):
+                if int(open(t_dir).read().strip()) > 0:
+                    t_m = '是'
+
+            u_m = '否'
+            u_dir = os.path.join(USPTO_DIR, keyword, '%s.txt' % asin)
+            if os.path.exists(u_dir):
+                if int(open(u_dir).read().strip()) > 0:
+                    u_m = '是'
+
+            products.append([title, asin, manufacturer, t_m, u_m])
+
+        fields = ['标题', 'asin', '品牌', 'TRADEMARKIA', 'USPTO']
+        csv_path = os.path.join(CSV_DIR, '%s.csv' % keyword)
+
+        with open(csv_path, 'w', encoding='utf-8', newline="") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(fields)
+            csvwriter.writerows(products)
 
 
-def start_list():
-    keywords = []
-    for file in os.listdir(KEYWORD_PATH):
-        # print(open(os.path.join(KEYWORD_PATH, file)).readlines())
-        keywords.extend([k.strip() for k in open(os.path.join(KEYWORD_PATH, file)).readlines() if k.strip()])
+def do_keyword(keyword):
+    try:
+        sa = SpiderAmazon()
+        sa.fetch_list(keyword)
+        sa.fetch_asins(keyword)
+        sa.fetch_trademarkia(keyword)
+        sa.fetch_uspto(keyword)
 
-    sa = SpiderAmazon(keywords)
-    sa.fetch_list()
-
-def start_asins():
-
-    sa = SpiderAmazon()
-    sa.fetch_asins()
+        sa.output_csv(keyword)
+    except Exception as ex:
+        traceback.print_exc()
 
 def start():
-    # start_list(keywords)
+    # for file in os.listdir(KEYWORD_PATH):
+    #     # print(open(os.path.join(KEYWORD_PATH, file)).readlines())
+    #     keywords.extend([k.strip() for k in open(os.path.join(KEYWORD_PATH, file)).readlines() if k.strip()])
+    # start_list()
     # start_asins()
-
 
     # sa.fetch_product('B09KWZXG2N')
 
-    sa = SpiderAmazon()
-    # sa.fetch_trademarkia()
-    sa.fetch_uspto()
+
+    executor = ThreadPoolExecutor(max_workers=5)
+
+    for file in os.listdir(KEYWORD_PATH):
+        for keyword in open(os.path.join(KEYWORD_PATH, file), 'r', encoding='utf-8').readlines():
+            keyword = keyword.strip()
+            if keyword:
+                executor.submit(do_keyword, keyword)
 
 
 
